b4ac13c807d60179b8b96d0238770602fe1144e1
emanuel caro <darkemlord@gmail.com>
2024-04-17 22:19:17 +0900

    refs #fixing form issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
+# Sessions Controller
 class SessionsController < ApplicationController
+  def show
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(session_params)
+    if @user.valid?
+      redirect_to root_path, notice: 'Session Started'
+    else
+      puts '=============='
+      puts sessions_path
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:user_id, :user_password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 # Table name: users
 class User < ApplicationRecord
   # alias_attribute :password_digest, :user_password
-  has_secure_password
-  validates :user_id, presence: true
-  validates :user_password, presence: true
+  # has_secure_password
+  validates :user_id, presence: { message: 'ウーザーIDを入力してください。' }
+  validates :user_password, presence: { message: 'パスワードを入力してください。' }
 end
