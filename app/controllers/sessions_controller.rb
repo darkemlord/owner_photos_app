90962587661687a33dab8048a8b4edc1e5b47176
emanuel caro <darkemlord@gmail.com>
2024-04-20 00:53:41 +0900

    refs: adding login erros flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,20 @@
 
 # Sessions Controller
 class SessionsController < ApplicationController
-  def new
-    @user = User.new
+  def create
+    @user = User.find_by(user_id: params[:user_id])
+    if @user.present? && @user.authenticate(params[:user_password])
+      # session[:user_id] = @user.id
+    else
+      redirect_to root_path, notice: login_errors
+    end
   end
 
-  def create
-    @user = User.new(session_params)
-    puts @user.valid?
-    if @user.valid?
-      redirect_to root_path, notice: 'Session Started'
-    else
-      render :new
-    end
+  def login_errors
+    error = []
+    error.push('ウーザーIDを入力してください。') if params[:user_id].blank?
+    error.push('パスワードを入力してください。') if params[:user_password].blank?
+    error
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 # Table name: users
 class User < ApplicationRecord
-  # alias_attribute :password_digest, :user_password
-  # has_secure_password
+  alias_attribute :password_digest, :user_password
+  has_secure_password
   validates :user_id, presence: { message: 'ウーザーIDを入力してください。' }
   validates :user_password, presence: { message: 'パスワードを入力してください。' }
 end
