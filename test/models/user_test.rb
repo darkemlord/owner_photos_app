043610a94865b8029cbbb9cb2fd59a08fa3d7c55
emanuel caro <darkemlord@gmail.com>
2024-04-21 21:03:32 +0900

    refs: User model test done

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
+
+# Tests for User model
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should save a valid user' do
+    user = User.new(user_id: 'user10', password: 'password')
+    assert user.valid?, 'The user was invalid'
+  end
+
+  test 'Should not accept user without user_id' do
+    user = User.new(user_id: '', password: 'password')
+    assert_not user.valid?, 'The user was valid.'
+  end
+
+  test 'Should not accept user without password' do
+    user = User.new(user_id: 'user11', password: '')
+    assert_not user.valid?, 'The user was valid.'
+  end
+
+  test 'Should not accept same user' do
+    user = User.new(user_id: 'user0', password: 'password')
+    assert_not user.valid?, 'The user was valid.'
+  end
 end
