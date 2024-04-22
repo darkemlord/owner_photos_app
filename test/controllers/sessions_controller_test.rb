e63c9659db541dc10b3ffb96899924329f17667a
emanuel caro <darkemlord@gmail.com>
2024-04-22 11:51:08 +0900

    refs: adding sessions controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -7,7 +7,27 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = User.create(user_id: 'user13', password: 'password')
   end
-  test 'Should authenticate user' do
-    assert @user.valid?
+  test 'Should get new' do
+    get new_session_path
+    assert_response :success
+  end
+
+  test 'should login with valid credentials' do
+    post sessions_path, params: { user_id: @user.user_id, password: 'password' }
+    assert_redirected_to photos_path
+    assert_equal @user.id, session[:user_id]
+  end
+
+  test 'should not login with invalid credentials' do
+    post sessions_path, params: { user_id: @user.user_id, password: 'wrong_password' }
+    assert_redirected_to sessions_path
+    assert_not_nil flash[:alert]
+    assert_nil session[:user_id]
+  end
+
+  test 'should logout user' do
+    delete logout_path
+    assert_nil session[:user_id]
+    assert_redirected_to root_path
   end
 end
